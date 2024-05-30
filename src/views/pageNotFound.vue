6e01c63eb3691a3f4e30404c565af9a7ce678dbb
Moutonlennie <12202563@student.pxl.be>
2024-05-30 09:28:57 +0200

    Page not found pagina

--- src/views/pageNotFound.vue
+++ src/views/pageNotFound.vue
@@ -1,4 +1,7 @@
 <template>
-    <h1>Deze pagina is niet gevonden!</h1>
-    <p>Ga naar <a href="@/homeView">de startpagina</a></p>
+    <div id="pageNotFound">
+        <h1>404</h1>
+        <h3>Deze pagina is niet gevonden</h3>
+        <p>Ga terug naar de <a href="http://localhost:5173/">startpagina</a></p>
+    </div>
 </template>
