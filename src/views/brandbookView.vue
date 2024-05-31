f38b20c70ee72eb8ff2f42220fd5d1de9a0b7f1b
Moutonlennie <12202563@student.pxl.be>
2024-05-31 10:28:16 +0200

    Brandbook

--- src/views/brandbookView.vue
+++ src/views/brandbookView.vue
@@ -96,27 +96,50 @@ export default {
                     <div id="container-card">
                         <cardComponent/>
                     </div>
-
+<br>
                     <h2>Varianten</h2>
                     <p>Geen card</p>
-
+<br>
                     <gebruikComponent/>
                 </div>
-
+<br><br>
                 <div id="kleuren">
                     <h1>Kleuren</h1>
                     <h2>Merkkleuren</h2>
-
+<br>
                     <h3>Primaire</h3>
-                    <p>Geen card</p>
-
+                    <p>Geen kleuren</p>
+<br>
                     <h3>Secundaire</h3>
-                    <p>Geen card</p>
-
+                    <p>Geen kleuren</p>
+<br>
                     <gebruikComponent/>
                 </div>
-
-                <div id="Typografie"></div>
+<br><br>
+                <div id="Typografie">
+                    <h1>Typografie</h1>
+                    <h2>Koppen</h2>
+                    <p>Geen typografie</p>
+<br>
+                    <h2>Tekst</h2>
+                    <p>Geen typografie</p>
+<br>
+                    <gebruikComponent/>
+                </div>
+<br><br>
+                <div id="Elementen">
+                    <h1>Elementen</h1>
+                    <p>Geen element gevonden</p>
+<br>
+                    <gebruikComponent/>
+                </div>
+<br><br>
+                <div id="Iconen">
+                    <h1>Iconen</h1>
+                    <p>Geen icoon gevonden</p>
+<br>
+                    <gebruikComponent/>
+                </div>
             </div>
         </div>
     </div>
