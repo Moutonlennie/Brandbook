551b7b5d8dff2b3e6224d15b072db3853ec9f6a8
Moutonlennie <12202563@student.pxl.be>
2024-05-31 13:33:52 +0200

    Button overlay

---
+++ src/componenten/buttonOverlay.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    data() {
+      return {
+        isPopupVisible: false
+      };
+    },
+    methods: {
+      hidePopup() {
+        this.isPopupVisible = false;
+      },
+      downloadImage(imageName) {
+        const link = document.createElement('a');
+        link.href = require(`@/assets/${imageName}`);
+        link.download = imageName;
+        link.click();
+      }
+    }
+  }
+</script>
+
+<template>
+    <div v-if="isPopupVisible" class="popup-overlay">
+        <div class="popup-content">
+          <h2>Digitaal</h2>
+          <button class="btn-sec" @click="downloadImage('digital1.jpg')">PNG <i class="bi bi-download"></i></button>
+          <button class="btn-sec" @click="downloadImage('digital2.jpg')">EPS <i class="bi bi-download"></i></button>
+          <button class="btn-sec" @click="downloadImage('digital3.jpg')">SVG <i class="bi bi-download"></i></button>
+          <button class="btn-sec" @click="downloadImage('digital3.jpg')">JPEG <i class="bi bi-download"></i></button>
+          
+          <br><br>
+          <h2>Print</h2>
+          <button class="btn-sec" @click="downloadImage('digital1.jpg')">PNG <i class="bi bi-download"></i></button>
+          <button class="btn-sec" @click="downloadImage('digital2.jpg')">EPS <i class="bi bi-download"></i></button>
+          <button class="btn-sec" @click="downloadImage('digital3.jpg')">SVG <i class="bi bi-download"></i></button>
+          <button class="btn-sec" @click="downloadImage('digital3.jpg')">JPEG <i class="bi bi-download"></i></button>
+  
+          <br>
+          <button class="btn-card" @click="hidePopup">Sluiten</button>
+        </div>
+    </div>
+</template>
+
--- src/componenten/cardComponent.vue
+++ src/componenten/cardComponent.vue
@@ -4,6 +4,7 @@ export default {
     data() {
         const store = logoStore();
         return {
+            isPopupVisible: false,
             store,
             logos: [],
         }
@@ -25,6 +26,20 @@ export default {
                 console.log(this.logos)
             }
         }
+    },
+    methods: {
+      showPopup() {
+        this.isPopupVisible = true;
+      },
+      hidePopup() {
+        this.isPopupVisible = false;
+      },
+      downloadImage(imageName) {
+        const link = document.createElement('a');
+        link.href = require(`@/assets/${imageName}`);
+        link.download = imageName;
+        link.click();
+      }
     }
 }
 </script>
@@ -37,13 +52,33 @@ export default {
             <div id="card-text">
                 <h1>{{ logo.title }}</h1>
             </div>
-            <button class="btn-card">Download</button>
+            <button class="btn-card" @click="showPopup">Download</button>
         </div>
+        <div v-if="isPopupVisible" class="popup-overlay">
+        <div class="popup-content">
+          <h2>Digitaal</h2>
+          <button class="btn-sec" @click="downloadImage('digital1.jpg')">PNG</button>
+          <button class="btn-sec" @click="downloadImage('digital2.jpg')">EPS</button>
+          <button class="btn-sec" @click="downloadImage('digital3.jpg')">SVG</button>
+          <button class="btn-sec" @click="downloadImage('digital3.jpg')">JPEG</button>
+          
+          <br><br>
+          <h2>Print</h2>
+          <button class="btn-sec" @click="downloadImage('digital1.jpg')">PNG</button>
+          <button class="btn-sec" @click="downloadImage('digital2.jpg')">EPS</button>
+          <button class="btn-sec" @click="downloadImage('digital3.jpg')">SVG</button>
+          <button class="btn-sec" @click="downloadImage('digital3.jpg')">JPEG</button>
+  
+          <br>
+          <button class="btn-card" @click="hidePopup">Sluiten</button>
+        </div>
+</div>
     </div>
 </div>
 <div v-else>
     <p>Geen card gevonden. <a href="http://localhost:5173/addlogo">Voeg een logo toe</a></p>
 </div>
+
     <!-- <div id="container-card">
         <div class="cards">
             <img class="card-img" src="@/assets/logo_pmo.png" alt="logo pmo">
